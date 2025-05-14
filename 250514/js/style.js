c262bdde32dba7ae5b8241c8fdfdcd33aaa16c6b
이상용 <lsy3709@naver.com>
2025-05-14 12:16:50 +0900

    4교시, 다중 선택된 취미 체크박스 내용 가져오기

--- 250514/js/style.js
+++ 250514/js/style.js
@@ -9,6 +9,9 @@ document.getElementById('signupForm').addEventListener('submit', function (e) {
     // 생성자를 호출한다고 함, 최종, 이렇게 표현을 하게 됨. 
     // this : 자기 자신 = document.getElementById('signupForm') 가리킴
     // formData 상수에는, 결론, 여러 기능이 들어 있다.
+    // 여기서
+    // 유저가 입력한 내용이 FormData 클래스에 담아져 있다. 
+    // 그래서, 우리는 각 요소를 하나씩 꺼내서 사용 중이다. 
     const formData = new FormData(this); // 폼 데이터 수집
 
     // const 상수명 = 값 :  값을 변경이 불가능함. 
@@ -18,17 +21,30 @@ document.getElementById('signupForm').addEventListener('submit', function (e) {
     // 단일 입력 필드 처리. 
     // 유저명 부터 표기 해보기. 
     // output = output + "내용"
-    // output += "내용" ,  같은 의미
+    // output += "내용" ,  같은 의미, 기존 내용에 새로운 내용을 계속 첨부하기.
     // x = 1
     // x = x + 1 <=> x += 1
 
     // 백틱 `, `문자열을 표기함` 
     // 1번째 요소, 유저명 가져오기
+    // formData.get('username') 의 역할, 이름이 username인 요소의 값을 가져오기
+    // 예시) ${formData.get('username')} -> lsy, 변환, 
     output += `출력되는 유저명 : ${formData.get('username')} \n`
     // 2번째 요소, 패스워드 가져오기, 
+    // 예시) ${formData.get('password')} -> 123456, 변환, 
     output += `출력되는 패스워드 : ${formData.get('password')}\n`
+    // 예시 output = 출력되는 유저명 lsy 줄넘김 출력되는 패스워드 : 123456
+
+    // 취미 , 다중 체크박스 처리 (hobby)
+    // 여러개 요소를 가져와서 , 배열에 담기
+    const hobbies = formData.getAll('hobby')
+    // hobbies 배열 = ['게임','독서','영화보기']
+    // hobbies.join(', ') =>  게임, 독서, 영화보기   이런 결과의 문자열됨. 
+    output += `출력되는 취미들: ${hobbies.join(', ')}\n`
+    // 출력되는 취미들: 게임, 독서, 영화보기
 
     // 공통 : 결과를 화면에 표기 하는 부분 연결 
+    //output의 내용을 , html 태그인 pre 태그에 출력한다.
     document.getElementById('output').textContent = output
 
 })
