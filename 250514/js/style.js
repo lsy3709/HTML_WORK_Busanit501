e4cdfd53e6ef87fdb25d6352affd861e7eb13f48
이상용 <lsy3709@naver.com>
2025-05-14 14:05:24 +0900

    5교시, 입력창의 내용들 , 결과 화면에 출력 해보기 확인

--- 250514/js/style.js
+++ 250514/js/style.js
@@ -43,6 +43,28 @@ document.getElementById('signupForm').addEventListener('submit', function (e) {
     output += `출력되는 취미들: ${hobbies.join(', ')}\n`
     // 출력되는 취미들: 게임, 독서, 영화보기
 
+    //다른 요소들도 가져오기
+    output += `출력되는 성별 : ${formData.get('gender')}\n`
+    output += `출력되는 나이 : ${formData.get('age')}\n`
+    output += `출력되는 생년월일 : ${formData.get('date')}\n`
+    output += `출력되는 이메일 : ${formData.get('email')}\n`
+
+    // 파일 첨부 확인. 
+    const file = formData.get('file')
+    // 삼항 연산자, 조건부 연산식 
+    // ${} : EL 표기법 (Expresion Language), 특정의 값을 출력할 때 자주 사용함.
+    // 조건식 : 
+    // 1) file 존재하고 
+    // 2) && : and 효과 
+    // 3)-1 (조건식) ? (참일때) :(거짓일때)
+    // 3)-2 file.name ? file.name :'파일없음'
+    // 3)-3 if(조건) {참일 때 실행될 문장} else{거짓일 때 실행될 문장}
+    // file && file.name ? file.name :'파일없음'
+    output += `출력되는 첨부파일명 : ${file && file.name ? file.name : '파일없음'}\n`
+
+    // 히든의 요소의 값도 같이 출력하기. user_id
+    output += `출력되는 히든의 요소 : ${formData.get('user_id')}\n`
+
     // 공통 : 결과를 화면에 표기 하는 부분 연결 
     //output의 내용을 , html 태그인 pre 태그에 출력한다.
     document.getElementById('output').textContent = output
